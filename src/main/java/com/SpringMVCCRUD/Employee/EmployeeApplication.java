90280ff5d78bc7f8db1dccb06acfce262b0287d7
alp <alpkarar0@gmail.com>
2023-11-14 17:26:23 +0300

    first-commit

---
+++ src/main/java/com/SpringMVCCRUD/Employee/EmployeeApplication.java
@@ -0,0 +1,22 @@
+package com.SpringMVCCRUD.Employee;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeApplication implements CommandLineRunner {
+
+	@Value("${server.port}")
+	private String port;
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Server started running on PORT " + port + "...");
+	}
+}
